e4251d4a9a409f48a146860f64dbbda597fe2ded
Tanvir Ahmad <tnvir9@gmail.com>
2024-03-21 00:56:15 +0600

    Added proptypes in Link.jsx

--- src/Components/Link/Link.jsx
+++ src/Components/Link/Link.jsx
@@ -1,6 +1,7 @@
-
+import PropTypes from 'prop-types';
 
 const Link = ({route}) => {
+  console.log(route);
   return (
     <li className="mr-8">
       <a href={route.path}>{route.name}</a>
@@ -8,4 +9,7 @@ const Link = ({route}) => {
   );
 };
 
+Link.propTypes = {
+  route: PropTypes.object.isRequired,
+}
 export default Link;
