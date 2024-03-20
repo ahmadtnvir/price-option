0c47f840dc60b8e3098a751a8685bc233904f30a
Tanvir Ahmad <tnvir9@gmail.com>
2024-03-21 01:21:04 +0600

    Created nav toggle and navbar animation

--- src/Components/Link/Link.jsx
+++ src/Components/Link/Link.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 const Link = ({route}) => {
-  console.log(route);
+  // console.log(route);
   return (
     <li className="mr-8">
       <a href={route.path}>{route.name}</a>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { HiMenuAlt1 } from "react-icons/hi";
+import { ImCross } from "react-icons/im";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   const routes = [
     { id: 1, name: "Home", path: "/" },
     { id: 2, name: "About", path: "/about" },
@@ -11,9 +15,16 @@ const Navbar = () => {
   return (
     <div>
       <nav>
-        <ul className="md:flex">
-          {routes.map((route) => <Link key={route.id} route={route}></Link>)
+        <div className=" md:hidden text-2xl" onClick={() => setOpen(!open)}>
+          {
+          
+            open === true ? <ImCross /> : <HiMenuAlt1/>
           }
+        </div>
+        <ul className="md:flex">
+          {routes.map((route) => (
+            <Link key={route.id} route={route}></Link>
+          ))}
         </ul>
       </nav>
     </div>
